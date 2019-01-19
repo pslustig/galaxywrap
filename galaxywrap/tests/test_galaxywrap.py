87fbfc3db8fd5d32433d9c0e684bd7e46992cd5e
Peter Lustig <peter.lustig@physik.lmu.de>
2019-01-19 06:12:42 +0100

    psf tests

--- galaxywrap/tests/test_galaxywrap.py
+++ galaxywrap/tests/test_galaxywrap.py
@@ -2,11 +2,18 @@ import galaxywrap as gw
 import astropy.units as u
 import numpy as np
 import pytest
+from astropy.convolution import Gaussian2DKernel
 
 
 def make_setup():
     setup = gw.setup(1, (1, 1) * u.arcsec, 'regular', 0)
     return setup
+
+
+def make_psf():
+    psf = Gaussian2DKernel(x_stddev=5, x_size=50)
+    psf = gw.psf(psf.array, (100, 100), 2)
+    return psf
 
 
 def test_setup_pixscale_scalar():
@@ -49,3 +56,6 @@ def test_runoption_invalid():
     for invalidoption in invalidoptions:
         with pytest.raises(AssertionError):
             setup.runoption = invalidoption
+
+def test_psf_init():
+    pass
