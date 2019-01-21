f0975c49f9b780cf0b179cc8914553af20a82622
Peter Lustig <peter.lustig@physik.lmu.de>
2019-01-21 18:33:03 +0100

    Added fixedm bool to parameters, added component, analytic component and sersic class

--- galaxywrap/models.py
+++ galaxywrap/models.py
@@ -3,11 +3,14 @@ from astropy.units import Quantity
 
 
 class parameter(object):
-    def __init__(self, value, uncertainty=None, name='', description=''):
+    def __init__(self, value, uncertainty=None, name='', description='',
+                 fixed=False):
         self.value = value
         self.uncertainty = uncertainty
         self.name = name
         self.description = description
+        assert isinstance(fixed, bool)
+        self.fixed = fixed
 
     @property
     def uncertainty(self):
@@ -34,8 +37,73 @@ class parameter(object):
                                         self.value, self.uncertainty)
 
 
-class sersic(object):
-    pass
+class component(object):
+    def __init__(self, name, x, y, mag, uncertainties, fixed):
+        self.name = name
+        self.x = parameter(x, uncertainties.pop('x', None), name='x',
+                           description='position x',
+                           fixed=fixed.pop('x', False))
+
+        self.y = parameter(y, uncertainties.pop('y', None), name='y',
+                           description='position y',
+                           fixed=fixed.pop('y', False))
+
+        self.mag = parameter(mag, uncertainties.pop('mag', None), name='mag',
+                             description='total magnitude',
+                             fixed=fixed.pop('mag', False))
+
+        self._parameters = [self.x, self.y, self.mag]
+
+    def to_galfit(self, skipinimage=False):
+        out = ' 0) {:25s} # object name'.format(self.name)
+        out += '\n 1) {:8.4f}  {:8.4f}  {:d}  {:d} # position x, y'.format(
+                        self.x.value, self.y.value, self.x.fixed, self.y.fixed)
+
+        for i, parameter in enumerate(self._parameters[2:]):
+            if parameter is not None:
+                out += '\n{:2d}) {:8.4f}         {:d}        # {}'.format(
+                            i+2, parameter.value, not parameter.fixed,
+                            parameter.description)
+        out += '\n Z) {:d}                         # {}'.format(
+                skipinimage, 'Skip this model in output image?(yes=1, no=0)')
+        return out
+
+
+class analytic_component(component):
+    def __init__(self, name, x, y, mag, r, ratio, pa, uncertainties,
+                 fixed):
+
+        super().__init__(name, x, y, mag, uncertainties, fixed)
+
+        self.r = parameter(r, uncertainties.pop('r'), name='r',
+                           description='effective radius',
+                           fixed=fixed.pop('r', False))
+
+        self.ratio = parameter(ratio, uncertainties.pop('ratio', None),
+                               name='ratio', description='axis ratio',
+                               fixed=fixed.pop('ratio', False))
+
+        self.pa = parameter(pa, uncertainties.pop('pa', None), name='pa',
+                            description='position angle',
+                            fixed=fixed.pop('pa', False))
+
+        self._parameters = [self.x, self.y, self.mag, self.r, None, None,
+                            None, None, self.ratio, self.pa]
+
+
+class sersic(analytic_component):
+    def __init__(self, x, y, mag, r, n, ratio, pa, uncertainties={}, fixed={}):
+        super().__init__('sersic', x, y, mag, r, ratio, pa, uncertainties,
+                         fixed)
+
+        self.n = parameter(n, uncertainties.pop('n', None), name='n',
+                           description='sersic index',
+                           fixed=fixed.pop('n', False))
+
+        self._parameters = [self.x, self.y, self.mag, self.r, self.n, None,
+                            None, None, self.ratio, self.pa]
+
+
 
 
 class sky(object):
--- galaxywrap/tests/test_models.py
+++ galaxywrap/tests/test_models.py
@@ -6,6 +6,7 @@ import pytest
 def test_parameter_init_value():
     p = mod.parameter(value=3)
     assert p.value == 3
+    assert p.unit is None
 
 
 def test_parameter_init_novalue():
@@ -27,3 +28,64 @@ def test_parameter_value_unit():
 def test_parameter__repr__():
     a = mod.parameter(value=3, uncertainty=1)
     assert a.__repr__() == 'parameter value: 3, uncertainty: 1'
+
+
+def test_parameter_change_unc_unit():
+    p = mod.parameter(5*u.kg, 500*u.g)
+    assert p.uncertainty.unit == u.kg
+
+    p.value = 3000 * u.g
+    assert p.uncertainty.unit == u.g
+
+
+def test_component_init():
+    c = mod.component('psf', 5000, 4, 20,
+                      uncertainties={'x': 1, 'y': 2, 'mag': 3},
+                      fixed={'x': True, 'y': True, 'mag': True})
+
+    assert c.x.value == 5000
+    assert c.y.value == 4
+    assert c.mag.value == 20
+
+    assert c.x.uncertainty == 1
+    assert c.y.uncertainty == 2
+    assert c.mag.uncertainty == 3
+
+    assert c.x.fixed is True
+    assert c.y.fixed is True
+    assert c.mag.fixed is True
+
+
+def test_component_to_galfit():
+    c = mod.component('psf', 5000, 4, 20,
+                      uncertainties={'x': 1, 'y': 2, 'mag': 3},
+                      fixed={'x': True, 'y': True, 'mag': True})
+
+    assert c.to_galfit() == ''' 0) psf                       # object name\n 1) 5000.0000    4.0000  1  1 # position x, y\n 2)  20.0000         0        # total magnitude\n Z) 0                         # Skip this model in output image?(yes=1, no=0)'''
+
+
+def test_analytic_component_init():
+    c = mod.analytic_component(
+                        'cname', 1, 2, 3, 4, 5, 6,
+                        uncertainties={'r': 4.5, 'ratio': 5.5, 'pa': 6.5},
+                        fixed={'r': True, 'ratio': True, 'pa': True})
+
+    assert c.r.value == 4
+    assert c.ratio.value == 5
+    assert c.pa.value == 6
+
+    assert c.r.uncertainty == 4.5
+    assert c.ratio.uncertainty == 5.5
+    assert c.pa.uncertainty == 6.5
+
+    assert c.r.fixed is True
+    assert c.ratio.fixed is True
+    assert c.pa.fixed is True
+
+
+def test_analytic_component_to_galfit():
+    c = mod.analytic_component(
+                        'cname', 1, 2, 3, 4, 5, 6,
+                        uncertainties={'r': 4.5, 'ratio': 5.5, 'pa': 6.5},
+                        fixed={'r': True, 'ratio': True, 'pa': True})
+    assert c.to_galfit() == ' 0) cname                     # object name\n 1)   1.0000    2.0000  0  0 # position x, y\n 2)   3.0000         1        # total magnitude\n 3)   4.0000         0        # effective radius\n 8)   5.0000         0        # axis ratio\n 9)   6.0000         0        # position angle\n Z) 0                         # Skip this model in output image?(yes=1, no=0)'
