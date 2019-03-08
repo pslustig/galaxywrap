2b5cd49fc8b3959e3b999d6a50fe9cc2b72641d8
Peter Lustig <peter.lustig@physik.lmu.de>
2019-03-08 16:23:11 +0100

    tried to set up sphinx

---
+++ docs/source/index.rst
@@ -0,0 +1,24 @@
+.. galaxywrap documentation master file, created by
+   sphinx-quickstart on Fri Feb 15 15:47:02 2019.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to galaxywrap's documentation!
+======================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+dsjgvseggj
+
+
+.. automodule:: galaxywrap.models
+   :members:
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
--- scripts/fit_example_file.py
+++ scripts/fit_example_file.py
@@ -17,10 +17,11 @@ with warnings.catch_warnings():
     img = gw.image.read(datadir/'gal.fits')
 
 psf = gw.psf(fits.getdata(datadir/'psf.fits'), (100, 100))
-component = gw.models.sersic(48.518, 51.2800, 20.0890, 5.1160, 4.2490, 0.7570, -60.3690)
+component = gw.models.sersic(48.518, 51.2800, 20.0890, 5.1160, 4.2490, 0.7570,
+                             -60.3690)
 model = gw.models.model(component)
 model.add_component(gw.models.sky(1.3920, 0, 0,
                                   fixed={'dbkg_dx': True, 'dbkg_dy': True}))
 
-res = model.fit(img, psf, '')
+res = model.fit(img, psf, '', deletefiles=False)
 res[0]
