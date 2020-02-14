2bd2581c41e346216687359e028afe4e77b7a1a2
Peter Lustig <peter.lustig@physik.lmu.de>
2020-02-14 12:27:31 +0100

    added command line script that reads galfit results from imgblock and prints component table

---
+++ scripts/read_results.py
@@ -0,0 +1,39 @@
+import galaxywrap as gw
+import argparse
+from astropy.table import Table, Column, vstack
+import numpy as np
+
+
+'''filename = ('/Users/peterlustig/Documents/fit_targets/manual/'
+            'id06_map08_mcc248503/add_psf/imgblock.fits')
+'''
+
+
+def remove_columns_with_patterns(table, patterns):
+    for k in table.keys():
+        if any([p in k for p in patterns]):
+            table.remove_column(k)
+
+
+parser = argparse.ArgumentParser(description='print galfit results')
+parser.add_argument('filenames', type=str, help='galfit imgblock filename(s)',
+                    nargs='*', default=['imgblock.fits'])
+parser.add_argument('-c', '--component', type=int, help='component to print')
+parser.add_argument('-u', action='store_true', help='print uncertainties')
+parser.add_argument('-f', action='store_true', help='print flags')
+args = parser.parse_args()
+
+patterns = [k for k, v in {'_unc': args.u, '_flag': args.f}.items() if not v]
+
+results = Table()
+for i, filename in enumerate(args.filenames):
+    result = gw.core.read_results(filename)['components']
+    result.add_column(Column(np.arange(1, len(result)+1)), name='N', index=0)
+    result.add_column(Column([i+1]*len(result)), name='fileno', index=0)
+    result = Table(result[args.component-1]) if args.component else result
+    results = vstack([results, result])
+
+remove_columns_with_patterns(results, patterns)
+
+results.pprint(max_lines=-1, max_width=-1, show_name=True,
+               show_unit=None, show_dtype=False, align=None)
